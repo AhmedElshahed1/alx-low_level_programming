ea440a861e9a01837db202c1a2d89992ca58d9d0
Ahmed <you@example.com>
2023-03-16 17:24:11 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,8 @@
 #include <unistd.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
+ *
  * Return : Always 1 
 */
 
@@ -10,3 +11,4 @@ int main(void)
 {
         write(2, "and that piece of art is usefil\n" - Dora Korpar, "2015-10-19\n" , 59);
         return (1);
+}
